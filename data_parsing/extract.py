003a0e80323427ebb4b4653527d2bd7cb28cdf54
Elijah Ang <eli.axel.ang@gmail.com>
2024-12-18 17:17:31 +0800

    pushing data_parsing updates to git

--- data_parsing/extract.py
+++ data_parsing/extract.py
@@ -1,26 +1,68 @@
+from typing import Any
+from re import sub
+
 from csv import DictReader
 from pathlib import Path
 
+from json import dump
+
 def main() -> None:
-    raw_data_folder = Path("./1_raw_data/")
+    raw_data_folder = Path("./raw_data/")
 
-    q_and_as = {
-
-    }
+    parsed_q_n_a: list[dict[str, Any]] = []
 
     for file_path in raw_data_folder.glob("./*"):
-        file_stem = file_path.stem
-
-        if file_stem.endswith("conversations"):
-            place_name = file_stem[:-len("_conversations")]
-            print(place_name)
+        if file_path.is_dir():
             continue
-            
-        with open(file_path, "r", newline="", encoding="utf-8") as place_q_and_a_file:
-            reader = DictReader(place_q_and_a_file)
 
-            for raw_q_and_a in reader:
-                print(raw_q_and_a)
+        with open(file_path, "r", newline="", encoding="utf-8") as q_n_a_csv:
+            reader = DictReader(q_n_a_csv)
+
+            for raw_q_n_a in reader:
+                raw_category = raw_q_n_a.pop("Category").lower().strip()
+
+                category_without_specials = sub(
+                    r"\[.*\]",
+                    "",
+                    raw_category
+                )
+
+                if raw_category != category_without_specials:
+                    continue
+
+                categories = (
+                    "_".join(category.split()) for
+                    category in
+                    (
+                        sub(
+                            r'\W+',
+                            ' ',
+                            category
+                        ) for
+                        category in
+                        category_without_specials.split(", ")
+                    )
+                )
+
+                cleaned_keys = {
+                    key.lower().replace(" ", "_"): value for
+                    key, value in
+                    raw_q_n_a.items() if 
+                    len(value) >= 1 and 
+                    all(
+                        (word not in key) for 
+                        word in
+                        ("Time", "Date") 
+                    )
+                }
+
+                for category in categories:
+                    cleaned_keys["category"] = category
+                    parsed_q_n_a.append(cleaned_keys)
+
+    with open("parsed.json", "w", encoding="utf-8") as parsed_json_file:
+        dump(parsed_q_n_a, parsed_json_file, indent=4)
+
 
 if __name__ == "__main__":
     main()
